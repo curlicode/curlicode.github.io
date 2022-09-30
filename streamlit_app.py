2a7ef0bb1c882ed9de8c17ac1101556fd3ea85c1
notroychu <roy.chu@gmail.com>
2022-09-30 01:40:21 -0700

    testing inputs

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,25 +6,26 @@ st.write("""
 Hello *world!*
 """)
 
+with st.siderbar:
 
-rate_interest = st.number_input(
-    'interest rate'.title()
-    ,min_value = 0.0
-    ,max_value = 20.0
-    ,value = 5.0
-    ,step = 0.1
-    ,format = "%f"
-    ,key = 'input-rate'
-    ,help = 'Selected interest rate'
-)
+    rate_interest = st.number_input(
+        'interest rate'.title()
+        ,min_value = 0.0
+        ,max_value = 20.0
+        ,value = 5.0
+        ,step = 0.1
+        ,format = "%f"
+        ,key = 'input-rate'
+        ,help = 'Selected interest rate'
+        )
 
-down_amount = st.number_input(
-    'down payment'.title()
-    ,min_value = 0.0
-    ,max_value = 100.0
-    ,value = 20.0
-    ,step = 0.1
-    ,format = "%f"
-    ,key = 'input-down'
-    ,help = 'Selected down payment amount'
-)
+    down_amount = st.number_input(
+        'down payment'.title()
+        ,min_value = 0.0
+        ,max_value = 100.0
+        ,value = 20.0
+        ,step = 0.1
+        ,format = "%f"
+        ,key = 'input-down'
+        ,help = 'Selected down payment amount'
+    )
