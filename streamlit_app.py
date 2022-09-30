6e14adc69640aca0d0693daaa100255db0eee713
notroychu <roy.chu@gmail.com>
2022-09-30 02:04:14 -0700

    testing inputs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.write("""
-# My first app
-Hello *world!*
-""")
+st.write('# Mortgage Assistant')
 
 with st.sidebar:
 
@@ -29,6 +26,22 @@ with st.sidebar:
         ,key = 'input-down'
         ,help = 'Selected down payment amount'
     )
+
+def calculate_amortization_amount(principal, interest_rate, periods):
+    x = (1 + interest_rate) ** periods
+    return principal * (interest_rate * x) / (x - 1)
+
+def amortization_schedule(principal, interest_rate, periods):
+    amortization_amount = calculate_amortization_amount(principal, interest_rate, periods)
+    number = 1
+    balance = principal
+    table = []
+    for period in range(periods):
+        interest = balance * interest_rate
+        principal = amortization_amount - interest
+        balance = balance - principal
+        table.append([(period + 1), amortization_amount, interest, principal, balance if balance > 0 else 0])
+    return table
 
 st.write(f"{rate_interest:.1f}")
 st.write(f"{down_amount:.1f}")
